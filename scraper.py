4d7a8f0669a5444f900fb2d8e95bcba3649b7011
Blacks <mj_ol@outlook.com>
2018-11-19 20:11:32 +0800

    UPDATE:Update scraper file to split data store files

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ import math,os
 records = 0
 pool = []
 
-url = "http://cq.zu.fang.com/house/i1-s31/"
+url = "http://cq.esf.fang.com/house/i31"
 
 header = {
     'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
@@ -24,14 +24,24 @@ def fuck_data():
     global url
     content = ""
     page = 31
+    fileno = '1'
     url = "http://cq.esf.fang.com/house/i"+str(page+30)
     requests.adapters.DEFAULT_RETRIES = 5
     s = requests.session()
     s.keep_alive = False
     #html = requests.get(url,headers = header)
     while True:
-        if records > 1000:
+        if fileno>10:
             break
+        if records > 10000:
+            print "file %s start" % fileno
+            fileno += 1
+            records = 0
+            f = open(fileno+'.html','wb')
+            f.write(content)
+            content = ""
+            f.close()
+
         #html = requests.get(url,headers = header,proxies = pool[random.randrange(len(pool))])
         try:
             html = s.get(url,headers = header)
@@ -49,11 +59,6 @@ def fuck_data():
             time.sleep(1)
             url = "http://cq.esf.fang.com/house/i"+str(page+30)
 
-    f = open('a.html','wb')
-    f.write(content)
-    f.close()
-        
-
 def deal_data_single(bsobj):
     #get data each page
     global records
@@ -70,6 +75,7 @@ def save_process():
     f = open("saved.txt",'wb')
     f.write(str(records))
     f.close()
+
 def retrieve_data():
     if os.path.exists('saved.txt'):
         f = open("saved.txt",'rb')
